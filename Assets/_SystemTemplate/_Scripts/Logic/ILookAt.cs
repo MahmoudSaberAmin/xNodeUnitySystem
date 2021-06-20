fe486d5e915929bd7cf0dc4f1ff58f31ba051e8a
MahmoudSaberAmin <46610744+MahmoudSaberAmin@users.noreply.github.com>
2021-06-20 09:30:16 +0200

    Deleted unused script

--- Assets/_SystemTemplate/_Scripts/Logic/ILookAt.cs
+++ Assets/_SystemTemplate/_Scripts/Logic/ILookAt.cs
@@ -29,7 +29,7 @@ public class LookAtSomething : ILookAt
             if (LookAtTransform == null)
             {
                 lookAtScript.ObjectToFollow = null;
-
+                 
             }
             else
             {
